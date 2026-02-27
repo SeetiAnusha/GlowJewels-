10170a143ea42d0142277467a3db11054d0d69ac
SeetiAnusha <seetianusha950@gmail.com>
2026-02-28 00:14:48 +0530

    added few product

--- src/App.tsx
+++ src/App.tsx
@@ -28,47 +28,423 @@ const heroImages = [
 const products = [
   {
     id: 1,
-    name: 'Royal Floral Necklace',
+    name: 'GIVA 925 Silver Rose Gold Necklace',
     category: 'Necklaces',
-    price: 89999,
-    originalPrice: 119999,
-    image: '/product-necklace.jpg',
-    rating: 4.9,
-    reviews: 234,
-    badge: 'Bestseller'
+    price: 3484,
+    originalPrice: 3484,
+    image: 'https://m.media-amazon.com/images/I/51SvnNKVyVL._SY575_.jpg',
+    rating: 4.5,
+    reviews: 0,
+    badge: 'New',
+    affiliateLink: 'https://amzn.to/4r1pqwC'
   },
   {
     id: 2,
-    name: 'Pearl Drop Earrings',
-    category: 'Earrings',
-    price: 24999,
-    originalPrice: 29999,
-    image: '/product-earrings.jpg',
-    rating: 4.8,
-    reviews: 189,
-    badge: 'New'
+    name: 'GIVA Heart Butterfly Pearl Necklace',
+    category: 'Necklaces',
+    price: 2849,
+    originalPrice: 3499,
+    image: 'https://m.media-amazon.com/images/I/51OJKwyv90L._SY625_.jpg',
+    rating: 4.5,
+    reviews: 128,
+    badge: 'Trending',
+    affiliateLink: 'https://amzn.to/400S5qF'
   },
   {
     id: 3,
-    name: 'Diamond Bangles Set',
-    category: 'Bangles',
-    price: 129999,
-    originalPrice: 149999,
-    image: '/product-bangles.jpg',
-    rating: 4.9,
-    reviews: 156,
-    badge: null
+    name: 'SALTY Anti Tarnish Vibrant Blue Butterfly Pendant Necklace',
+    category: 'Necklaces',
+    price: 649,
+    originalPrice: 999,
+    image: 'https://m.media-amazon.com/images/I/61nswNNZpPL._SY625_.jpg',
+    rating: 4.3,
+    reviews: 95,
+    badge: 'Best Seller',
+    affiliateLink: 'https://amzn.to/4tV2TVb'
   },
   {
     id: 4,
-    name: 'Eternal Diamond Ring',
-    category: 'Rings',
-    price: 54999,
-    originalPrice: 69999,
-    image: '/product-ring.jpg',
-    rating: 5.0,
-    reviews: 312,
-    badge: 'Popular'
+    name: 'MEENAZ Heart Locket Gold Stainless Steel Pendant Necklace',
+    category: 'Necklaces',
+    price: 299,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/41vMdpGSy7L._SY695_.jpg',
+    rating: 4.4,
+    reviews: 210,
+    badge: 'Popular',
+    affiliateLink: 'https://amzn.to/4ba95kB'
+  },
+  {
+    id: 5,
+    name: "Nilu's Collection Stylish CZ Chain Pendant Set",
+    category: 'Necklaces',
+    price: 297,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/51NSY-FMDgL._SX695_.jpg',
+    rating: 4.6,
+    reviews: 156,
+    badge: 'New',
+    affiliateLink: 'https://amzn.to/4s9AngE'
+  },
+  {
+    id: 6,
+    name: "Nilu's Collection 18K Rose Gold Plated Heart Pendant",
+    category: 'Necklaces',
+    price: 299,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/41ayA5cSlDL._SX695_.jpg',
+    rating: 4.5,
+    reviews: 89,
+    badge: 'Valentine Special',
+    affiliateLink: 'https://amzn.to/3N9rgxD'
+  },
+  {
+    id: 7,
+    name: 'Fashion Frill Four Leaf Pendant Jewellery',
+    category: 'Necklaces',
+    price: 299,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/416puKorgUL._SY625_.jpg',
+    rating: 4.2,
+    reviews: 67,
+    badge: 'Hot Deal',
+    affiliateLink: 'https://amzn.to/4benc8r'
+  },
+  {
+    id: 8,
+    name: 'SALTY Anti Tarnish Dancing Swan Pendant Necklace',
+    category: 'Necklaces',
+    price: 349,
+    originalPrice: 699,
+    image: 'https://m.media-amazon.com/images/I/61fvMVuXWqL._SY625_.jpg',
+    rating: 4.6,
+    reviews: 142,
+    badge: 'Trending',
+    affiliateLink: 'https://amzn.to/4rLFXpK'
+  },
+  {
+    id: 9,
+    name: 'GIVA 925 Silver Rose Gold Infinity Pendant Set',
+    category: 'Jewelry Sets',
+    price: 1998,
+    originalPrice: 2999,
+    image: 'https://m.media-amazon.com/images/I/51rxoDaZ3vL._SY625_.jpg',
+    rating: 4.7,
+    reviews: 203,
+    badge: 'Premium',
+    affiliateLink: 'https://amzn.to/3OE0rCa'
+  },
+  {
+    id: 10,
+    name: 'SALTY Anti Tarnish Dreamy Heart Pendant Necklace',
+    category: 'Necklaces',
+    price: 499,
+    originalPrice: 799,
+    image: 'https://m.media-amazon.com/images/I/51yVUiRFHkL._SY695_.jpg',
+    rating: 4.4,
+    reviews: 118,
+    badge: 'Valentine Special',
+    affiliateLink: 'https://amzn.to/4rHgwpq'
+  },
+  {
+    id: 11,
+    name: 'Yellow Chimes Silver Butterfly Pendant with Purple Crystal',
+    category: 'Necklaces',
+    price: 288,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/51FlHbCjLhL._SY695_.jpg',
+    rating: 4.3,
+    reviews: 87,
+    badge: 'Best Value',
+    affiliateLink: 'https://amzn.to/4skSroh'
+  },
+  {
+    id: 12,
+    name: 'SALTY Anti Tarnish Fancy Loving Gift Box Set',
+    category: 'Jewelry Sets',
+    price: 1599,
+    originalPrice: 2499,
+    image: 'https://m.media-amazon.com/images/I/71W20022KbL._SY625_.jpg',
+    rating: 4.8,
+    reviews: 176,
+    badge: 'Gift Set',
+    affiliateLink: 'https://amzn.to/40EO0sl'
+  },
+  {
+    id: 13,
+    name: 'Shining Diva AAA Crystal Butterfly Chain Pendant',
+    category: 'Necklaces',
+    price: 289,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/51HKS6d7QaL._SY695_.jpg',
+    rating: 4.4,
+    reviews: 134,
+    badge: 'Best Seller',
+    affiliateLink: 'https://amzn.to/4tX9e26'
+  },
+  {
+    id: 14,
+    name: 'MEENAZ Moti Shell Pearl Rose Gold Long Chain',
+    category: 'Necklaces',
+    price: 197,
+    originalPrice: 499,
+    image: 'https://m.media-amazon.com/images/I/51YU60HM7DL._SY625_.jpg',
+    rating: 4.5,
+    reviews: 98,
+    badge: 'Budget Pick',
+    affiliateLink: 'https://amzn.to/402UWzr'
+  },
+  {
+    id: 15,
+    name: 'Yellow Chimes Multilayer Pearl Choker Set',
+    category: 'Jewelry Sets',
+    price: 288,
+    originalPrice: 699,
+    image: 'https://m.media-amazon.com/images/I/61RrH1p-0BL._SY695_.jpg',
+    rating: 4.6,
+    reviews: 167,
+    badge: 'Trending',
+    affiliateLink: 'https://amzn.to/4l9c14n'
+  },
+  {
+    id: 16,
+    name: 'I Jewels Traditional Stone Pearl Choker Set',
+    category: 'Jewelry Sets',
+    price: 399,
+    originalPrice: 799,
+    image: 'https://m.media-amazon.com/images/I/81sKlabB8dL._SY625_.jpg',
+    rating: 4.7,
+    reviews: 189,
+    badge: 'Traditional',
+    affiliateLink: 'https://amzn.to/46zoVCO'
+  },
+  {
+    id: 17,
+    name: 'Shining Diva Fancy Pearl Choker Traditional Set',
+    category: 'Jewelry Sets',
+    price: 598,
+    originalPrice: 999,
+    image: 'https://m.media-amazon.com/images/I/71L18OHjSsL._SY695_.jpg',
+    rating: 4.8,
+    reviews: 221,
+    badge: 'Premium',
+    affiliateLink: 'https://amzn.to/4aTZaOD'
+  },
+  {
+    id: 18,
+    name: 'Shining Diva 18k Gold & Silver Pearl Jewellery Set',
+    category: 'Jewelry Sets',
+    price: 283,
+    originalPrice: 699,
+    image: 'https://m.media-amazon.com/images/I/616eAeTsWWL._SY695_.jpg',
+    rating: 4.5,
+    reviews: 156,
+    badge: 'Designer',
+    affiliateLink: 'https://amzn.to/3MSGRS0'
+  },
+  {
+    id: 19,
+    name: 'Sasitrends Traditional Gold Plated Pearl Necklace Set',
+    category: 'Jewelry Sets',
+    price: 830,
+    originalPrice: 1499,
+    image: 'https://m.media-amazon.com/images/I/81wGxwa3rnL._SY675_.jpg',
+    rating: 4.6,
+    reviews: 145,
+    badge: 'Traditional',
+    affiliateLink: 'https://amzn.to/4sfPbdN'
+  },
+  {
+    id: 20,
+    name: 'Shining Diva Pearl Bracelet Earrings Necklace Set',
+    category: 'Jewelry Sets',
+    price: 494,
+    originalPrice: 999,
+    image: 'https://m.media-amazon.com/images/I/715x4rLGktL._SY695_.jpg',
+    rating: 4.7,
+    reviews: 198,
+    badge: 'Valentine Gift',
+    affiliateLink: 'https://amzn.to/3OFXOQf'
+  },
+  {
+    id: 21,
+    name: 'Shining Diva Latest Choker Design Necklace Set',
+    category: 'Jewelry Sets',
+    price: 299,
+    originalPrice: 699,
+    image: 'https://m.media-amazon.com/images/I/71t65utyG6L._SY695_.jpg',
+    rating: 4.5,
+    reviews: 167,
+    badge: 'Best Seller',
+    affiliateLink: 'https://amzn.to/4aEUspj'
+  },
+  {
+    id: 22,
+    name: 'Yellow Chimes Temple Bridal Choker Necklace Set',
+    category: 'Jewelry Sets',
+    price: 698,
+    originalPrice: 1299,
+    image: 'https://m.media-amazon.com/images/I/71r7vM0GusL._SY695_.jpg',
+    rating: 4.8,
+    reviews: 234,
+    badge: 'Bridal',
+    affiliateLink: 'https://amzn.to/4ceqWb6'
+  },
+  {
+    id: 23,
+    name: 'Shining Diva Traditional Fancy Necklace Set',
+    category: 'Jewelry Sets',
+    price: 398,
+    originalPrice: 799,
+    image: 'https://m.media-amazon.com/images/I/71pFMNcYCxL._SY695_.jpg',
+    rating: 4.6,
+    reviews: 189,
+    badge: 'Trending',
+    affiliateLink: 'https://amzn.to/47haN19'
+  },
+  {
+    id: 24,
+    name: 'Shining Diva 18k Gold Plated Choker Necklace Set',
+    category: 'Jewelry Sets',
+    price: 348,
+    originalPrice: 699,
+    image: 'https://m.media-amazon.com/images/I/71zDzsfZWeL._SY695_.jpg',
+    rating: 4.5,
+    reviews: 156,
+    badge: 'Popular',
+    affiliateLink: 'https://amzn.to/4qZ9FWU'
+  },
+  {
+    id: 25,
+    name: 'Shining Diva Pearl Wedding Party Choker Set',
+    category: 'Jewelry Sets',
+    price: 389,
+    originalPrice: 799,
+    image: 'https://m.media-amazon.com/images/I/716OR461pAL._SY695_.jpg',
+    rating: 4.7,
+    reviews: 201,
+    badge: 'Wedding',
+    affiliateLink: 'https://amzn.to/4r70Iep'
+  },
+  {
+    id: 26,
+    name: 'Sasitrends Traditional Pearl Chain Pendant Set',
+    category: 'Jewelry Sets',
+    price: 915,
+    originalPrice: 1699,
+    image: 'https://m.media-amazon.com/images/I/813S1Ob6abL._SY695_.jpg',
+    rating: 4.8,
+    reviews: 178,
+    badge: 'Premium',
+    affiliateLink: 'https://amzn.to/4rLL8pG'
+  },
+  {
+    id: 27,
+    name: 'ZENEME American Diamond Leaf Shaped Necklace Set',
+    category: 'Jewelry Sets',
+    price: 609,
+    originalPrice: 1299,
+    image: 'https://m.media-amazon.com/images/I/719+X33tXkL._SY695_.jpg',
+    rating: 4.6,
+    reviews: 143,
+    badge: 'Complete Set',
+    affiliateLink: 'https://amzn.to/4tVc6Nf'
+  },
+  {
+    id: 28,
+    name: 'MEENAZ Heart Lock Triple Layer Chain Choker',
+    category: 'Necklaces',
+    price: 333,
+    originalPrice: 699,
+    image: 'https://m.media-amazon.com/images/I/61bpIKxltqL._SY625_.jpg',
+    rating: 4.5,
+    reviews: 167,
+    badge: 'Valentine Gift',
+    affiliateLink: 'https://amzn.to/3OMA4tJ'
+  },
+  {
+    id: 29,
+    name: 'MEENAZ Oxidised Silver Chandbali Pearl Jhumka',
+    category: 'Earrings',
+    price: 168,
+    originalPrice: 399,
+    image: 'https://m.media-amazon.com/images/I/910LFbNPhhL._SY625_.jpg',
+    rating: 4.7,
+    reviews: 289,
+    badge: 'Best Seller',
+    affiliateLink: 'https://amzn.to/4aGdGuF'
+  },
+  {
+    id: 30,
+    name: 'MEENAZ Butterfly Peacock Temple Jhumki Earrings',
+    category: 'Earrings',
+    price: 299,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/91ApOe7A3RL._SY695_.jpg',
+    rating: 4.6,
+    reviews: 234,
+    badge: 'Traditional',
+    affiliateLink: 'https://amzn.to/3MKdQbj'
+  },
+  {
+    id: 31,
+    name: 'Zaveri Pearls Pink Kundan Austrian Diamond Earrings',
+    category: 'Earrings',
+    price: 278,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/61PihX8SyoL._SY695_.jpg',
+    rating: 4.5,
+    reviews: 198,
+    badge: 'Ethnic',
+    affiliateLink: 'https://amzn.to/4tZRKST'
+  },
+  {
+    id: 32,
+    name: 'YouBella Gold Plated Pearl Jhumka Earrings',
+    category: 'Earrings',
+    price: 289,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/6148o0F+OaL._SY695_.jpg',
+    rating: 4.4,
+    reviews: 176,
+    badge: 'Traditional',
+    affiliateLink: 'https://amzn.to/3OCU02g'
+  },
+  {
+    id: 33,
+    name: 'Zaveri Pearls Antique Gold Tone Traditional Jhumki',
+    category: 'Earrings',
+    price: 282,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/61TC+8FXvaL._SY695_.jpg',
+    rating: 4.7,
+    reviews: 212,
+    badge: 'Antique',
+    affiliateLink: 'https://amzn.to/46xOEvk'
+  },
+  {
+    id: 34,
+    name: 'Rubans Oxidised Jhumka Earrings Stylish Traditional',
+    category: 'Earrings',
+    price: 262,
+    originalPrice: 599,
+    image: 'https://m.media-amazon.com/images/I/61U6ezaLUlL._SY695_.jpg',
+    rating: 4.5,
+    reviews: 189,
+    badge: 'Trendy',
+    affiliateLink: 'https://amzn.to/4bd3gml'
+  },
+  {
+    id: 35,
+    name: 'Estele Fancy Designer Sparkling Nakshatra Earrings',
+    category: 'Earrings',
+    price: 314,
+    originalPrice: 699,
+    image: 'https://m.media-amazon.com/images/I/61yD6uKXatL._SY695_.jpg',
+    rating: 4.6,
+    reviews: 156,
+    badge: 'Designer',
+    affiliateLink: 'https://amzn.to/4aF2gY9'
   }
 ]
 
@@ -486,8 +862,13 @@ function App() {
                   <Heart className="w-4 h-4 text-glow-pink-500" />
                 </motion.button>
 
-                {/* Image */}
-                <div className="relative aspect-square overflow-hidden bg-glow-pink-50">
+                {/* Image - Clickable to Affiliate Link */}
+                <a 
+                  href={product.affiliateLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="relative aspect-square overflow-hidden bg-glow-pink-50 block cursor-pointer"
+                >
                   <motion.img
                     src={product.image}
                     alt={product.name}
@@ -495,7 +876,7 @@ function App() {
                     whileHover={{ scale: 1.1 }}
                     transition={{ duration: 0.5 }}
                   />
-                </div>
+                </a>
 
                 {/* Content */}
                 <div className="p-5">
@@ -522,16 +903,21 @@ function App() {
                     </span>
                   </div>
 
-                  {/* Add to Cart */}
-                  <motion.button
-                    onClick={addToCart}
-                    className="w-full py-3 bg-glow-pink-500 text-white font-medium rounded-xl hover:bg-glow-pink-600 transition-colors flex items-center justify-center gap-2"
-                    whileHover={{ scale: 1.02 }}
-                    whileTap={{ scale: 0.98 }}
+                  {/* View Product - Affiliate Link */}
+                  <a
+                    href={product.affiliateLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    onClick={(e) => {
+                      console.log('Button clicked! Link:', product.affiliateLink);
+                      // Let the default link behavior happen
+                    }}
+                    className="relative z-10 block w-full py-3 bg-glow-pink-500 text-white font-medium rounded-xl hover:bg-glow-pink-600 transition-all hover:scale-[1.02] active:scale-[0.98] text-center no-underline cursor-pointer"
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '0.5rem', textDecoration: 'none' }}
                   >
                     <ShoppingBag className="w-4 h-4" />
-                    Add to Cart
-                  </motion.button>
+                    View Product
+                  </a>
                 </div>
               </motion.div>
             ))}
